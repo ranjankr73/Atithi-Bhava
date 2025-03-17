b238da1d34ce708fa1898b01a56ec22fe9c5008c
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-17 13:46:45 +0530

    add few photos

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#75D3CF',
+        'secondary': '#2D9596',
+        'accent': '#FF6B6B',
+        'accent-light': '#FFE1E1',
+        'dark': '#2D3436',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif']
+      },
+      spacing: {
+        '18': '4.5rem',
+      }
+    },
   },
   plugins: [],
 }
