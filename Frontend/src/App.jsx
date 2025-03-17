b03ecc30113546783cf084842ced38c68ab14949
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-17 20:29:07 +0530

    add toaster in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import { RouterProvider } from './routes'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   return (
     <>
       <RouterProvider/>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          className: 'font-open-sans',
+          success: {
+            className: 'border-2 border-green-500',
+            iconTheme: {
+              primary: '#4CAF50',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            className: 'border-2 border-red-500'
+          },
+        }}
+      />
     </>
   )
 }
