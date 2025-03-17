9134f2a4028f3e58b8a13df5300efb06247f3362
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-17 20:23:29 +0530

    add cors config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,11 +5,14 @@ const eventRoute = require('./routes/event.route');
 const app = express();
 
 //Middlewares
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 
 //Routes
-// app.use('/api/v1/user', userRoute);
+app.use('/api/v1/user', userRoute);
 app.use('/api/v1/event', eventRoute);
 // app.use('/api/v1/guest', guestRoute);
 
